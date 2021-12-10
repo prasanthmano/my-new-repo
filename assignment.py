8c0acd7a0fe66282d97ecfd8c2d5f93eee8c4e63
prasanth manoharan <91420455+prasanthmano@users.noreply.github.com>
2021-12-10 16:34:24 +0530

    uploaded Assignment operator file
    I uploaded assgnment operator file please check it...

---
+++ assignment.py
@@ -0,0 +1,77 @@
+#assignment operator  =, +=. -=, *=, /=, %=, //=, **=, &=, ^=, |=,<<=, >>=
+a=20
+print(a)
+a=20
+a+=20
+print(a)
+
+b=30
+b-=20
+print(b)
+
+c=40
+c*=20
+print(c)
+
+d=50
+d/=20
+print(d)
+
+e=60
+e%=20
+print(e)
+
+f=70
+f**=20
+print(f)
+
+g=80
+g//=20
+print(g)
+
+h=90
+h&=20
+print(h)
+
+i=100
+i|=20
+print(i)
+
+j=10
+j^=20
+print(j)
+
+k=20
+k<<=4
+print(k)
+
+l=30
+l>>=4
+print(l)
+
+
+
+print("*******************************")
+
+
+#ternary operator / conditional operator
+
+money=10; coins=20
+result=10 if money<coins else 20
+print(result)
+
+
+print("*******************************")
+
+#differents between and vs &
+
+print(5 & 7)         # 5
+print(5 and 7)       # 7
+
+print(5 and True)
+print(5 or 7)
+print(5 or False)
+print(False or 5)
+print(False and 5)
+
+
